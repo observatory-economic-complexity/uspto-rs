79158fb77098f01d52212bac4ee5fae70b50695d
Walther Chen <walther.chen@gmail.com>
2019-08-05 17:07:20 -0400

    refactor data structure to have less nesting, now it doesn't have to mirror xml

--- src/data.rs
+++ src/data.rs
@@ -22,29 +22,26 @@ pub struct PatentGrant {
 
 #[derive(Debug, Default)]
 pub struct BibliographicDataGrant {
-    pub publication_reference: PublicationReference,
-    pub application_reference: ApplicationReference,
+    pub publication_reference: DocumentId,
+    pub application_reference: DocumentId,
     pub us_application_series_code: String,
-    pub us_term_of_grant: LengthOfGrant,
+    pub us_term_of_grant: String,
     pub classification_locarno: ClassificationLocarno,
     pub classification_national: ClassificationNational,
     pub invention_title: InventionTitle,
 //    pub us_references_cited: Vec<UsCitation>,
     pub number_of_claims: String,
-    pub us_field_of_classifciation_search: UsFieldOfClassificationSearch,
-    pub us_parties: UsParties,
-    pub assignees: Assignees,
+    pub us_field_of_classification_search: UsFieldOfClassificationSearch,
+
+    // ==================
+    // Us Parties
+    pub us_applicants: Vec<UsApplicant>,
+    pub inventors: Vec<Inventor>,
+    pub agents: Vec<Agent>,
+    // ==================
+
+    pub assignees: Vec<Assignee>,
     pub examiners: Examiners,
-}
-
-#[derive(Debug, Default)]
-pub struct PublicationReference {
-    pub document_id: DocumentId,
-}
-
-#[derive(Debug, Default)]
-pub struct ApplicationReference {
-    pub document_id: DocumentId,
 }
 
 #[derive(Debug, Default)]
@@ -53,11 +50,6 @@ pub struct DocumentId {
     pub doc_number: String,
     pub kind: Option<String>,
     pub date: String,
-}
-
-#[derive(Debug, Default)]
-pub struct LengthOfGrant {
-    pub length_of_grant: String,
 }
 
 #[derive(Debug, Default)]
@@ -85,18 +77,6 @@ pub struct UsFieldOfClassificationSearch {
     pub classification_cpc_text: Vec<String>,
 }
 
-#[derive(Debug, Default)]
-pub struct UsParties {
-    pub us_applicants: UsApplicants,
-    pub inventors: Inventors,
-    pub agents: Agents,
-}
-
-#[derive(Debug, Default)]
-pub struct UsApplicants {
-    pub us_applicants: Vec<UsApplicant>,
-}
-
 #[derive(Debug)]
 pub struct UsApplicant {
     pub sequence: String,
@@ -104,7 +84,7 @@ pub struct UsApplicant {
     pub designation: String,
     pub applicant_authority_category: String,
     pub addressbook: AddressBook,
-    pub residence: Residence,
+    pub residence: String, // Country
 }
 
 #[derive(Debug)]
@@ -113,24 +93,11 @@ pub struct AddressBook {
     pub first_name: Option<String>,
     pub last_name: Option<String>,
     pub role: Option<String>,
-    pub address: Address,
-}
 
-#[derive(Debug)]
-pub struct Address{
+    // Address
     pub city: Option<String>,
     pub state: Option<String>,
     pub country: Option<String>,
-}
-
-#[derive(Debug)]
-pub struct Residence{
-    pub country: String,
-}
-
-#[derive(Debug, Default)]
-pub struct Inventors {
-    pub inventors: Vec<Inventor>,
 }
 
 #[derive(Debug)]
@@ -140,21 +107,11 @@ pub struct Inventor {
     pub addressbook: AddressBook,
 }
 
-#[derive(Debug, Default)]
-pub struct Agents {
-    pub agents: Vec<Agent>,
-}
-
 #[derive(Debug)]
 pub struct Agent {
     pub sequence: String,
     pub rep_type: String,
     pub addressbook: AddressBook,
-}
-
-#[derive(Debug, Default)]
-pub struct Assignees {
-    pub assignee: Vec<Assignee>,
 }
 
 #[derive(Debug)]
@@ -164,13 +121,13 @@ pub struct Assignee {
 
 #[derive(Debug, Default)]
 pub struct Examiners {
-    pub primary_examiner: PrimaryExaminer,
+    pub primary_examiner: Examiner,
 
     // TODO are there other examiners?
 }
 
 #[derive(Debug, Default)]
-pub struct PrimaryExaminer {
+pub struct Examiner {
     pub first_name: String,
     pub last_name: String,
     pub department: String,
